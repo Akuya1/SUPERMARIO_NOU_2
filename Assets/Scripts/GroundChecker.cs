d93a11903792c0b8affea6edb3bd6a5f4b679d07
Akuya <aiyuegonzalbes@gmail.com>
2022-01-27 13:54:05 +0100

    summit
    Update

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    Player _player;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _player = GameObject.Find("Mario").GetComponent<Player>();
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        _player.isGrounded = true;
+        _player._animator.SetBool("Jumping", false);
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        _player.isGrounded = false;
+    }
+}
